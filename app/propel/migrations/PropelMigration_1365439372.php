27a860f51bd46088ed69d21762bcf8c6ea030f04
Bernardo <bernardosecades@gmail.com>
2013-04-09 08:59:46 +0200

    Dependency

---
+++ app/propel/migrations/PropelMigration_1365439372.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * Data object containing the SQL and PHP code to migrate the database
+ * up to version 1365439372.
+ * Generated on 2013-04-08 18:42:52 by bernardo
+ */
+class PropelMigration_1365439372
+{
+
+    public function preUp($manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postUp($manager)
+    {
+        // add the post-migration code here
+    }
+
+    public function preDown($manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postDown($manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * Get the SQL statements for the Up migration
+     *
+     * @return array list of the SQL strings to execute for the Up migration
+     *               the keys being the datasources
+     */
+    public function getUpSQL()
+    {
+        return array (
+  'default' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+CREATE TABLE `product`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100),
+    `price` DECIMAL,
+    `description` TEXT,
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+    }
+
+    /**
+     * Get the SQL statements for the Down migration
+     *
+     * @return array list of the SQL strings to execute for the Down migration
+     *               the keys being the datasources
+     */
+    public function getDownSQL()
+    {
+        return array (
+  'default' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS `product`;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+    }
+
+}
