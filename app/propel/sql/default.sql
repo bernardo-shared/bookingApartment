f5914407aee777ff84433e4e1d2ff29c107e4674
Bernardo <bernardosecades@gmail.com>
2013-04-09 11:21:37 +0200

    Enabled web/bundles

--- app/propel/sql/default.sql
+++ app/propel/sql/default.sql
@@ -4,17 +4,99 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ---------------------------------------------------------------------
--- product
+-- flat
 -- ---------------------------------------------------------------------
 
-DROP TABLE IF EXISTS `product`;
+DROP TABLE IF EXISTS `flat`;
 
-CREATE TABLE `product`
+CREATE TABLE `flat`
 (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(100),
-    `price` DECIMAL,
-    `description` TEXT,
+    `description` VARCHAR(100),
+    `country` VARCHAR(100),
+    `city` VARCHAR(100),
+    `postcode` VARCHAR(6),
+    `street` VARCHAR(30),
+    `number` VARCHAR(5),
+    `floor` VARCHAR(5),
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- room
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `room`;
+
+CREATE TABLE `room`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `description` VARCHAR(100),
+    `flat_id` INTEGER,
+    PRIMARY KEY (`id`),
+    INDEX `room_FI_1` (`flat_id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- bed
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `bed`;
+
+CREATE TABLE `bed`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `type` VARCHAR(10),
+    `room_id` INTEGER,
+    PRIMARY KEY (`id`),
+    INDEX `bed_FI_1` (`room_id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- customer
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `customer`;
+
+CREATE TABLE `customer`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(10),
+    `email` VARCHAR(100),
+    `password` VARCHAR(100),
+    PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- booking
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `booking`;
+
+CREATE TABLE `booking`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `start_date` DATE,
+    `end_date` DATE,
+    `customer_id` INTEGER,
+    `bed_id` INTEGER,
+    PRIMARY KEY (`id`),
+    INDEX `booking_FI_1` (`customer_id`),
+    INDEX `booking_FI_2` (`bed_id`)
+) ENGINE=MyISAM;
+
+-- ---------------------------------------------------------------------
+-- user
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user`
+(
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(10),
+    `email` VARCHAR(100),
+    `password` VARCHAR(100),
     PRIMARY KEY (`id`)
 ) ENGINE=MyISAM;
 
