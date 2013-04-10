8518c1d333f1d6db7613f5dbf20db0b987a8b027
Bernardo <bernardosecades@gmail.com>
2013-04-10 09:27:31 +0200

    Backend CRUD

--- src/Tsp/AdminBundle/Controller/FlatController.php
+++ src/Tsp/AdminBundle/Controller/FlatController.php
@@ -4,7 +4,10 @@ namespace Tsp\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Tsp\AdminBundle\Model\Flat;
+use Tsp\AdminBundle\Model\FlatQuery;
+use Tsp\AdminBundle\Form\FlatType;
 
+// http://propelorm.org/cookbook/symfony2/mastering-symfony2-forms-with-propel.html
 
 class FlatController extends Controller
 {
@@ -14,38 +17,74 @@ class FlatController extends Controller
         return $this->render('AdminBundle:Flat:index.html.twig', array('name' => $name));
     }
 
-//    public function newAction()
-//    {
-//        $entity = new Category  ();
-//        $form   = $this->createForm(new CategoryType(), $entity);
-//
-//        return $this->render('AdminBundle:Flat:new.html.twig', array(
-//            'entity' => $entity,
-//            'form'   => $form->createView()
-//        ));
-//    }
-//
-//    public function createAction()
-//    {
-//        $entity  = new Category();
-//        $request = $this->getRequest();
-//        $form    = $this->createForm(new CategoryType(), $entity);
-//        $form->bindRequest($request);
-//
-//        if ($form->isValid()) {
-//            $em = $this->getDoctrine()->getEntityManager();
-//            $em->persist($entity);
-//            $em->flush();
-//
-//            return $this->redirect($this->generateUrl('category_show', array('id' => $entity->getId())));
-//
-//        }
-//
-//        return $this->render('AdminBundle:Category:new.html.twig', array(
-//            'entity' => $entity,
-//            'form'   => $form->createView()
-//        ));
-//    }
+    public function newAction()
+    {
+        $model = new Flat();
+        $form = $this->createForm(new FlatType(), $model);
+
+        return $this->render('AdminBundle:Flat:new.html.twig', array(
+            'model' => $model,
+            'form'   => $form->createView()
+        ));
+    }
+
+    public function createAction()
+    {
+        $flat  = new Flat();
+        $request = $this->getRequest();
+        $form    = $this->createForm(new FlatType(), $flat);
+
+        if ('POST' === $request->getMethod()) {
+
+            $form->bindRequest($request);
+
+            if ($form->isValid()) {
+                $flat->save();
+                return $this->redirect($this->generateUrl('show_flat', array('id' => $flat->getId())));
+            }
+        }
+
+        return $this->render('AdminBundle:Flat:show.html.twig', array(
+            'form'   => $form->createView() // I the view get flat -> $flat = $form->getData()
+        ));
+    }
+
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $entity = $em->getRepository('PostBundle:Category')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Category entity.');
+        }
+
+        $editForm = $this->createForm(new CategoryType(), $entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('AdminBundle:Category:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    public function showAction($id)
+    {
+        $flat = FlatQuery::create()->findPk($id);
+
+        if (!$flat) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return $this->render('AdminBundle:Flat:show.html.twig', array(
+            'flat' => $flat
+        ));
+
+    }
+
 //
 //    public function editAction($id)
 //    {
